74a427a2f176b9c4e8cd7ff92051e2d4ad4eb887
Anand Raut <anandraut7794@gmail.com>
2025-07-20 01:43:52 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -8,18 +8,8 @@ const app = express()
 await connectDB()
 
 // Middlewares
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
 
+app.use(cors( {origin: "*"}))
 app.use(express.json())
 
 //Routes
@@ -30,3 +20,4 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> {
     console.log(`Server running at ${PORT}`)
 })
+//
